04ccc46a2c2d34da3c7fb01fb00a1814c57aba7a
Derion Williams <derionwllms@gmail.com>
2022-02-13 15:30:10 -0600

    Completed a working version of an 'Etch-A-Sketch'

--- script.js
+++ script.js
@@ -1,25 +1,13 @@
 let gridSize = 16;
 
 const container = document.getElementById('container');
+const clearBtn = document.getElementById('clear');
+const boxes = document.getElementsByClassName('box');
 
 container.style.display = "grid";
-container.style.gridTemplateColumns = `repeat( ${gridSize}, 1fr)`;
-container.style.gridTemplateRows = `repeat( ${gridSize}, ${85/gridSize}vh)`;
-
-let num =1;
-
-for (let rows = 0; rows < gridSize; rows++){
-    for (let columns = 0; columns < gridSize; columns++){
-        const div = document.createElement('div');
-        div.style.border = "solid #000000";
-        div.classList.add('box')
-        //div.textContent = num;
-        num++;
-        container.appendChild(div);
-    }
-}
-
-let hoveredBox;
+container.style.width = "95vw";
+container.style.height = "95vh";
+container.style.border = "solid #000000";
 
 container.onmouseover = function(event){
     let target = event.target;
@@ -27,10 +15,45 @@ container.onmouseover = function(event){
     highlight(target);
 }
 
+clearBtn.addEventListener('click', clearBoard);
+
+function createBoard(size){
+    for (let rows = 0; rows < gridSize; rows++){
+        for (let columns = 0; columns < gridSize; columns++){
+            const div = document.createElement('div');
+            div.style.border = "solid #EFEFEF";
+            div.style.borderWidth = "1px";
+            div.classList.add('box');
+            container.appendChild(div);
+        }
+    }
+    container.style.gridTemplateColumns = `repeat( ${gridSize}, ${100/gridSize}%)`;
+    container.style.gridTemplateRows = `repeat( ${gridSize}, ${100/gridSize}%)`;
+}
+
+function deleteBoard(){
+    while(boxes.length >0){
+        boxes[0].parentNode.removeChild(boxes[0]);
+    }
+}
+
+function clear(e){
+    if(!(e.classList.contains("hovered"))) return;
+    e.classList.remove("hovered");
+}
+
+function clearBoard(){
+    Array.from(boxes).forEach(clear);
+    gridSize = prompt("how many boxes do you want on each side?");
+    deleteBoard();
+    createBoard(gridSize);
+}
+
 function highlight(div){
-    selectedDiv = div;
-    if(selectedDiv){
+    if(div){
         div.classList.remove('hovered');
     }
     div.classList.add('hovered');
 }
+
+createBoard(16);
